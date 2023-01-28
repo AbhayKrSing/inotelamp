b8ffb72eb749403dd75f393e1e742bf59f1e5772
Abhay <kabhay849@gmail.com>
2023-01-29 01:33:11 +0530

    lecture 46 completed express-validator used to perform schema validation

--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -1,17 +1,29 @@
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
+const { body, validationResult } = require('express-validator');
 mongoose.set('strictQuery', true)
-const User=require('../models/User.js')
-const express=require('express')
-router=express.Router()
+const User = require('../models/User.js')
+const express = require('express')
+router = express.Router()
 
 //Create a User using:POST "/api/auth".Doesn't require Auth
-router.post('/',(req,res)=>{
-    user=new User(req.body)
-    user.save().then((value)=>{
-        res.send(value)
+router.post('/', [
+    body('name','Write a valid name').isAlpha(),
+    body('email',"Write a valid email address").isEmail(),
+    body('password','Write a password whose length must be till 8 letters').isLength({ min: 8 })
+], (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array()});
+    }
+    user = new User(req.body)
+    user.save().then((value) => {
+        res.json(value)
         console.log('data saved')
-    }).catch((err)=>{
-       console.log(err.message)
-    })  
+    }).catch((err) => {
+        console.log(err.message)
+        res.status(400).send('Write unique email address')
+    })
 })
-module.exports=router
+module.exports = router
+
+//Note: if you write duplicate email is get passed from middleware(express-validator) but later on the promise use.save() will get rejected because schema(or model) will not followed due to this duplicate error is shown in console.

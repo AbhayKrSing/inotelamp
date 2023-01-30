dddd283fb676b13aa5d1cb071f4df04a31bdd718
Abhay <kabhay849@gmail.com>
2023-01-31 00:23:47 +0530

    password hashing done

--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -1,33 +1,38 @@
 const mongoose = require('mongoose')
 const { body, validationResult } = require('express-validator');
 mongoose.set('strictQuery', true)
+const bcrypt = require('bcryptjs');
 const User = require('../models/User.js')
 const express = require('express')
 router = express.Router()
 
 //Create a User using:POST "/api/auth".No login required
 router.post('/createuser', [
-    body('name', 'Write a valid name').isAlpha('en-US',{ignore: " "}),             //adding express-validator
+    body('name', 'Write a valid name').isAlpha('en-US', { ignore: " " }),             //adding express-validator
     body('email', "Write a valid email address").isEmail(),
     body('password', 'Write a password whose length must be till 8 letters').isLength({ min: 8 })
 ], async (req, res) => {
     //if express-validator founds error ,errors.isEmpty() becomes false and code inside it runs and code will end with error response.
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array()});     //expres-validator error syntax
+        return res.status(400).json({ errors: errors.array() });     //expres-validator error syntax
     }
-    
+
     try { //checking if email already exists
-        user=await User.findOne({email : req.body.email})
-        if(user){
-            return res.status(400).json({error:'This email already exists'})
+        user = await User.findOne({ email: req.body.email })
+        if (user) {
+            return res.status(400).json({ error: 'This email already exists' })
         }
+        var salt = bcrypt.genSaltSync(10);    //synchronous
+        // var salt =await bcrypt.genSalt(10);  //It is promise
+        var secpass = bcrypt.hashSync("B4c0/\/", salt);   //synchronous
+        // var secpass = await bcrypt.hash("B4c0/\/", salt);  ////It is promise
+        req.body.password=secpass
         //Saving data to database
         user = new User(req.body)
-        data= await user.save()
+        data = await user.save()
+        console.log(data)
         res.send(data)
-        console.log('data saved')
-    
     } catch (error) {
         console.log(error.message)
         res.send(400).send('Some error occured')

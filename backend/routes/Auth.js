56815b34f3d38d363af545da24482b141e7ec85f
Abhay <kabhay849@gmail.com>
2023-02-16 00:26:26 +0530

    creating login endpoints and sending authtoken

--- backend/routes/Auth.js
+++ backend/routes/Auth.js
@@ -6,8 +6,8 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/User.js')
 const express = require('express')
 router = express.Router()
-const JWT_seceret='Helloifavmovieisteth$dam' //ideally it must be in local environment(env.local)
-//Create a User using:POST "/api/auth".No login required
+const JWT_seceret = 'Helloifavmovieisteth$dam' //ideally it must be in local environment(env.local)
+//Route 1)Create a User using:- using POST "/api/auth/createuser"(No login required)
 router.post('/createuser', [
     body('name', 'Write a valid name').isAlpha('en-US', { ignore: " " }),             //adding express-validator
     body('email', "Write a valid email address").isEmail(),
@@ -27,18 +27,18 @@ router.post('/createuser', [
         var salt = bcrypt.genSaltSync(10);    //synchronous
         // var salt =await bcrypt.genSalt(10);  //It is promise
         var secpass = bcrypt.hashSync(req.body.password, salt);   //synchronous
-        // var secpass = await bcrypt.hash(req.body.password, salt);  ////It is promise
+        // var secpass = await bcrypt.hash(req.body.password, salt);  //It is promise
         req.body.password = secpass
         //Saving data to database
         user = new User(req.body)
         user = await user.save()
         console.log(user)
         console.log('data saved')
-        data={
-            id:user.id
+        data = {
+            id: user.id
         }
         const authtoken = jwt.sign(data, JWT_seceret); //jwt is json web token (please visit website for more details or refer harry video)
-          res.json({authtoken})
+        res.json({ authtoken })
     } catch (error) {
         console.log(error.message)
         res.status(400).send('Some error occured')
@@ -53,6 +53,46 @@ router.post('/createuser', [
     //     res.status(400).send('Write unique email address')
     // })
 })
+//Route 2) Login:- using POST "/api/auth/login"(login required)
+router.post('/login', [
+    body('email', 'Write a valid credentials').isEmail(),
+    body('password', 'Write a valid credentials').exists()
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });     //expres-validator error syntax
+    }
+    try {
+        user = await User.findOne({ email: req.body.email })
+        if (!user) {
+            return res.status(400).json({ error: 'Please login with correct credentials' })
+        }
+        let passwordcompare=bcrypt.compareSync(req.body.password,user.password) //synchronous method
+        if(!passwordcompare){
+            return res.status(400).json({error:'Please login with correct credentials'})
+        }
+        // console.log(req.body.password,user.password)
+        data = {
+            id: user.id
+        }
+        const authtoken = jwt.sign(data, JWT_seceret); 
+        res.json({authtoken})
+    }
+    catch (error) {
+        console.log(error.message)
+        res.status(400).send('Error in a interval server')
+    }
+})
+//Route 3) Login:- using POST "/api/auth/getuser"(login required)
+router.post('/getuser',async(req,res)=>{
+    try {
+     user= await User.findById()    
+    }
+     catch (error) {
+        
+    }
+})
 module.exports = router
 
 //Note: if you write duplicate email is get passed from middleware(express-validator) but later on the promise use.save() will get rejected because schema(or model) will not followed due to this duplicate error is shown in console.
+//please note ,I actually removed indexing of email in schema.(so above statement will not work)

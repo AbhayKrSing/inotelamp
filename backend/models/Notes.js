32e4acd7233d4845825b15c5d788fba7c9b802d5
Abhay <kabhay849@gmail.com>
2023-02-19 23:12:01 +0530

    fetching all notes and adding a Note

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -1,22 +1,27 @@
-import mongoose, { model } from 'mongoose';
+const mongoose=require('mongoose')
 const { Schema } = mongoose;
 
 const notesSchema = new Schema({
-   title:{
-    type:String,
-    required:true
-   },
-   description:{
-    type:String,
-    required:true
-   },
-   tag:{
-     type:String,
-   },
-   date:{
-    type:String,
-    default:new Date()
-   }
-   
+  user: {
+    type: mongoose.Schema.Types.ObjectId, //It make sure that one user cannot see others notes i.e user that logedin can only see his notes
+    ref: 'User'
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  tag: {
+    type: String,
+    default:'Personal'
+  },
+  date: {
+    type: String,
+    default: new Date()
+  }
+
 });
-module.exports=mongoose.model('User',notesSchema)
+module.exports = mongoose.model('Notes', notesSchema)
--- backend/routes/Notes.js
+++ backend/routes/Notes.js
@@ -1,9 +1,36 @@
-const express=require('express')
-router=express.Router()
+const express = require('express')
+router = express.Router()
+const Note = require('../models/Notes.js')
+const fetchuser = require('../middleware/fetchuser.js')
+const { body, validationResult } = require('express-validator');
+//Route:1  Fetch all user notes  using GET "/api/notes/fetchallnotes"( login required)
+router.get('/fetchallnotes', fetchuser, async (req, res) => {
+    try {
+        const notes = await Note.find({user:req.user.id})
+        res.json(notes)
+    } catch (error) {
+        console.log(error.message)
+        res.status(400).send('Some error occured')
+    }
 
-router.get('/',(req,res)=>{
-    res.json({
-        'notes':'learning react JS'
-    })
 })
-module.exports=router
+//Route:2 Creating or Adding notes using POST "/api/notes/creatingnotes"(login required)
+router.post('/creatingnotes', fetchuser, [
+    body('title', 'Write a valid title name').isLength({ min: 3 }),             //adding express-validator
+    body('description', "Write a description of minimum length having 8 characters").isLength({ min: 8 }),
+], async (req, res) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });     //expres-validator error syntax
+        }
+        const { title, tag, description } = req.body                   //destructuring
+        const notes = new Note({ user: req.user.id, title, description, tag })
+        const savednotes = await notes.save()
+        res.json(savednotes)
+    } catch (error) {
+        console.log(error.message)
+        res.status(400).send('Some error occured')
+    }
+})
+module.exports = router

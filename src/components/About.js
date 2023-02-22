d4f20e874fc4388f7ff3f076314ec03e341beba8
Abhay <kabhay849@gmail.com>
2023-02-22 18:13:23 +0530

    improved code in About.js component

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,10 @@
 
-import React ,{useContext,useEffect} from 'react'
-import NoteContext from '../context/notes/noteContext'
+import React  from 'react'
 export default function About() {
-  const a=useContext(NoteContext)
-  useEffect(() => {
-    a.update()
-    //eslint-disable-next-line
-  }, [])
-  
   return (
    <>
    <div>
-    <h1 className='text-center'>I m {a.state.name} and I m in {a.state.class} class</h1>
+    <h1 className='text-center'>This is notelamp for storing notes</h1>
    </div>
    </>
   )

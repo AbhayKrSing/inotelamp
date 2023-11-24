348ec0383c32768b986e339731a6fbc2988dc159
Abhay <kabhay849@gmail.com>
2023-11-24 18:19:44 +0530

    backend part ready for deployment

--- backend/db.js
+++ backend/db.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', true)
-mongoUrl = 'mongodb://127.0.0.1:27017/notelamp'
+
 
 let connectTomongo = () => {
-  mongoose.connect(process.env.MONGO_URL, (err) => {
+  mongoose.connect(process.env.Mongo_URL, (err) => {
     if (!err) console.log("Connected")
     else console.log(err.message)
   })
